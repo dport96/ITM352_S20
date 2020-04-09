62019e66c3337886838c465f4e65a2e04ba6de01
Dan Port <dport96@gmail.com>
2020-04-09 13:20:34 -1000

    xxx

---
+++ Lab13/info_server_Ex2.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var myParser = require("body-parser");
+
+app.all('*', function (request, response, next) {
+    console.log(request.method + ' to ' + request.path);
+    next();
+});
+
+app.use(myParser.urlencoded({ extended: true }));
+app.post("/order_page.html", function (request, response) {
+   let POST = request.body;
+   response.send(POST); 
+});
+
+app.use(express.static('./public'));
+app.listen(8090, () => console.log(`listening on port 8090 ex2`));
